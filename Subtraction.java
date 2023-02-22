7aa9596fe8cece38401e3f6c3eaa43ee82e66e0e
monikap22 <monapatel0922@gmail.com>
2023-02-21 23:17:35 -0500

    Subtraction file added

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class Subtraction {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 4;
+		int c = a - b ;
+		System.out.println(" The value of a - b is : " + c );
+     }
+}
