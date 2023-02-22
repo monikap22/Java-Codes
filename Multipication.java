3d0ccc9d1a5c6e507065f95539e0d3b62613c739
monikap22 <monapatel0922@gmail.com>
2023-02-22 00:01:56 -0500

    Multiplication file added

---
+++ Multipication.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class Multipication {
+
+	public static void main(String[] args) {
+		int a = 26;
+		int b = 6;
+		int c = a * b;
+		System.out.println(" The value of a * b is : " +c);
+	}
+
+}
