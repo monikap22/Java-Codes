820c79401df35802d35675a84eef415dc7d96538
monikap22 <monapatel0922@gmail.com>
2023-02-22 00:08:51 -0500

    Division file added

---
+++ Division.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class Division {
+
+	public static void main(String[] args) {
+		int x  = 98;
+		int y = 24;
+		int z = x/y;
+		System.out.println("The value of x / y is : " +z);
+
+	}
+
+}
