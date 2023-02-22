aea527ba285bcbf64f47e7d2de7580f3552a4948
monikap22 <monapatel0922@gmail.com>
2023-02-21 22:51:25 -0500

    Console Output and Addition

---
+++ Addition.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+public class Addition {
+
+	public static void main(String[] args) {
+	  int w = 3;
+	  int x = 2;
+	  int y = 6;
+	  int z = w+x+y;
+	System.out.println("The value of w + x + y is : " + z);
+	  
+	}
+	
+}
---
+++ ConsoleOutput.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class ConsoleOutput {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+
+}
