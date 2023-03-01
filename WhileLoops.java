cbdc642147cad8477817324fe7a45727e92150be
monikap22 <monapatel0922@gmail.com>
2023-02-28 21:52:08 -0500

    WhileLoops file added

---
+++ WhileLoops.java
@@ -0,0 +1,19 @@
+package helloworld;
+
+public class WhileLoops {
+
+	public static void main(String[] args) {
+		// The code below counts from 1 to 15
+		int i = 1;
+		while (i<= 15) {
+			System.out.println(i);
+			i++ ;
+			// add one to i
+			//substract 1 from i 
+		}
+	}
+
+}
+
+	
+
